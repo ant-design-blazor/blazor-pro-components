87866f2393ab61330f82913d5a067cbe2ddf583b
yoli <799480165@qq.com>
2020-09-13 18:30:39 +0800

    fix backup button style when empty

--- src/PageContainer/PageContainer.razor.cs
+++ src/PageContainer/PageContainer.razor.cs
@@ -4,28 +4,17 @@ namespace AntDesign.Pro.Layout
 {
     public partial class PageContainer
     {
-        private const string prefixCls = "ant-pro";
-        private string prefixedClassName = $"{prefixCls}-page-container";
-        private RenderFragment _breadcrumb;
-        private readonly RenderFragment _emptyBackIcon = builder => { };
+        private const string PrefixCls = "ant-pro";
+        private readonly string _prefixedClassName = $"{PrefixCls}-page-container";
 
-        [Parameter]
-        public RenderFragment ExtraContent { get; set; }
+        [Parameter] public RenderFragment ExtraContent { get; set; }
 
-        [Parameter]
-        public RenderFragment Content { get; set; }
+        [Parameter] public RenderFragment Content { get; set; }
 
-        [Parameter]
-        public RenderFragment ChildContent { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
 
-        [Parameter]
-        public string Title { get; set; }
+        [Parameter] public string Title { get; set; }
 
-        [Parameter]
-        public RenderFragment Breadcrumb
-        {
-            get => _breadcrumb ?? ((builder) => { });
-            set => _breadcrumb = value;
-        }
+        [Parameter] public RenderFragment Breadcrumb { get; set; }
     }
 }
