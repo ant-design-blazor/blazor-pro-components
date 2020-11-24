4522ff166c7142dd70fae4ebdf17d218f9486b4e
zhaowenkai <799480165@qq.com>
2020-11-24 11:13:07 +0800

    upgrade to .net 5.0

--- example/AntDesign.Pro.Layout.Wasm/Program.cs
+++ example/AntDesign.Pro.Layout.Wasm/Program.cs
@@ -1,8 +1,8 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace AntDesign.Pro.Layout.Wasm
 {
@@ -11,7 +11,7 @@ namespace AntDesign.Pro.Layout.Wasm
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddAntDesign();
