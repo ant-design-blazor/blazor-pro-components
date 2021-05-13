f1dc9bedc87cfe51bdcee3887b3a6da8bb505226
James Yeung <shunjiey@hotmail.com>
2021-05-13 23:12:13 +0800

    rebase master

--- src/layout/src/RightContent/AvatarDropdown.razor.cs
+++ src/layout/src/RightContent/AvatarDropdown.razor.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 
 namespace AntDesign.ProLayout
 {
@@ -7,5 +8,25 @@ namespace AntDesign.ProLayout
         [Parameter] public string Avatar { get; set; }
         [Parameter] public string Name { get; set; }
         [Parameter] public EventCallback<MenuItem> OnItemSelected { get; set; }
+
+        [Parameter]
+        public IEnumerable<AvatarMenuItem> MenuItems { get; set; } = 
+        new AvatarMenuItem[]
+        {
+            new AvatarMenuItem { Key = "center", IconType = "user", Option = "个人中心"},
+            new AvatarMenuItem { Key = "setting", IconType = "setting", Option = "个人中心"},
+            new AvatarMenuItem { IsDivider = true },
+            new AvatarMenuItem { Key = "logout", IconType = "logout", Option = "个人中心"}
+        };
     }
+
+    public class AvatarMenuItem
+    {
+        public string Key { get; set; }
+        public string IconType { get; set; }
+        public string IconTheme { get; set; } = "outline";
+        public string Option { get; set; }
+        public bool IsDivider { get; set; }
+    }
+
 }
