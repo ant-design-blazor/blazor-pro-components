0e81cabefc25ba3eee9b205778f0a4c3eae0b4c2
James Yeung <shunjiey@hotmail.com>
2021-07-29 17:58:15 +0800

    fix null reference exception (#32)

--- src/layout/src/BasicLayout.razor.cs
+++ src/layout/src/BasicLayout.razor.cs
@@ -31,7 +31,7 @@ namespace AntDesign.ProLayout
         [Parameter] public MenuDataItem[] MenuData { get; set; }
         [Parameter] public MenuMode Mode { get; set; }
         [Parameter] public EventCallback<bool> OnCollapse { get; set; }
-        [Parameter] public string[] OpenKeys { get; set; }
+        [Parameter] public string[] OpenKeys { get; set; } = { };
         [Parameter] public MenuTheme Theme { get; set; }
         [Parameter] public OneOf<string, RenderFragment> Logo { get; set; }
         [Parameter] public int SiderWidth { get; set; } = 208;
--- src/layout/src/SiderMenu/SiderMenu.razor.cs
+++ src/layout/src/SiderMenu/SiderMenu.razor.cs
@@ -34,7 +34,7 @@ namespace AntDesign.ProLayout
         [Parameter] public MenuDataItem[] MenuData { get; set; }
         [Parameter] public MenuMode Mode { get; set; } = MenuMode.Inline;
         [Parameter] public EventCallback<bool> OnCollapse { get; set; }
-        [Parameter] public string[] OpenKeys { get; set; }
+        [Parameter] public string[] OpenKeys { get; set; } = { };
         [Parameter] public OneOf<string, RenderFragment> Logo { get; set; }
         [Parameter] public int SiderWidth { get; set; } = 208;
         [Parameter] public BreakpointType Breakpoint { get; set; } = BreakpointType.Lg;
